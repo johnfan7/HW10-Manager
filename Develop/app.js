e034d41a8874e100df8768f66977cf27e56a94b7
John Fan <jf3287@columbia.edu>
2020-11-04 19:32:24 -0500

    added addeng & addintern functions

--- Develop/app.js
+++ Develop/app.js
@@ -83,7 +83,176 @@ function appMenu() {
             fs.createTeam();
         });
     }
+
+
+    function createTeam() {
+
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "memberChoice",
+                message: "Which type of team member would you like to add?",
+                choice: [
+                    "Engineer",
+                    "Intern",
+                    "I don't want to add any more team members"
+                ]
+            }
+        ]).then(userChoice => {
+            switch(userChoice.memberChoice) {
+                case "Engineer":
+                    addEngineer();
+                    break;
+                case "Intern":
+                    addIntern();
+                    break;
+                default:
+                    buildTeam();
+            }
+        });
+    }
+
+    function addEngineer() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "engineerName",
+                message: "What is your engineer's name?",
+                validate: answer => {
+                    if(answer !== "") {
+                        return true;
+                    }
+                    return "Please enter at least one character.";
+                }
+            },
+
+            {
+                type: "input",
+                name: "engineerId",
+                message: "What is your engineer's id?",
+                validate: answer => {
+                    const pass = answer.match(
+                        /^[1-9]\d*$/
+                    );
+                    if (pass) {
+                        if (idArray.includes(answer)) {
+                            return "This ID is already taken. Please enter a differnet number"
+                        } else {
+                            return true;
+                        }
+                    }
+                    return "Please enter a positive number greater than zero.";
+                }
+
+            },
+            {
+                type: "input",
+                name: "engineerEmail",
+                message: "What is your engineer's email?",
+                validate: answer => {
+                    const pass = answer.match(
+                        /\S+@\S+\.\S+/
+                    );
+                    if (pass) {
+                        return true;
+                    }
+                    return "Please enter a valid email address.";
+                }
+
+            },
+
+            {
+
+                type: "input",
+                name: "engineerGithub",
+                message: "What is your engineer's Github?",
+                validate: answer => {
+                    if(answer !== "") {
+                        return true;
+                    }
+                    return "Please enter at least one character.";
+                    
+                }
+
+            }
+
+
+        ])
+
+    }
+
+    function addIntern() {
+
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "internName",
+                message: "What is your intern's name?",
+                validate: answer => {
+                    if(answer !== "") {
+                        return true;
+                    }
+                    return "Please enter at least one character.";
+                    
+                }
+
+            },
+
+            {
+                type: "input",
+                name: "internId",
+                message: "What is your intern's id?",
+                validate: answer => {
+                    const pass = answer.match(
+                        /^[1-9]\d*$/
+                    );
+                    if (pass) {
+                        if (idArray.includes(answer)) {
+                            return "This ID is already taken. Please enter a differnet number"
+                        } else {
+                            return true;
+                        }
+                    }
+                    return "Please enter a positive number greater than zero.";
+                }
+
+            },
+            {
+                type: "input",
+                name: "internEmail",
+                message: "What is your intern's email?",
+                validate: answer => {
+                    const pass = answer.match(
+                        /\S+@\S+\.\S+/
+                    );
+                    if (pass) {
+                        return true;
+                    }
+                    return "Please enter a valid email address.";
+                }
+
+            },
+
+            {
+
+                type: "input",
+                name: "internSchool",
+                message: "What is your intern's School?",
+                validate: answer => {
+                    if(answer !== "") {
+                        return true;
+                    }
+                    return "Please enter at least one character.";
+                    
+                }
+
+            }
+        ])
+    }
+
+
 }
+
 
 
 
